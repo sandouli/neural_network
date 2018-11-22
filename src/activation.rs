e7e54c820087354161c788d8335ac2e287ec5ae9
sandouli <calvez.freddy@gmail.com>
2018-11-22 10:20:40 +0100

    Relocate activation function computation

--- src/activation.rs
+++ src/activation.rs
@@ -1,6 +1,33 @@
+use ndarray::Array2;
+
+
 pub enum Activation {
+    Identity,
+    Binary(f64),
     Sigmoid,
     TanH,
     ReLU,
 }
 
+
+impl Activation {
+    pub fn compute(&self, array: Array2<f64>) -> Array2<f64> {
+        match *self {
+            Activation::Identity => {
+                array
+            },
+            Activation::Binary(threshold) => {
+                array.map(|v| if v < &threshold { 0.0 } else { 1.0 })
+            }
+            Activation::Sigmoid => {
+                array
+            },
+            Activation::TanH => {
+                array.map(|v| v.tanh())
+            },
+            Activation::ReLU => {
+                array
+            },
+        }
+    }
+}
--- src/layer.rs
+++ src/layer.rs
@@ -13,9 +13,18 @@ pub struct Layer {
 
 impl Layer {
     pub fn new(neurons: usize, inputs: usize, activation_function: Activation) -> Self {
+
+        // Create matrix with random weights and add one row for the bias weight
+        let mut weights = Array2::<f64>::random((inputs + 1, neurons), Range::new(0.0, 1.0));
+
+        // Initialize the last row with the bias weight (equal or close to zero)
+        let bias_weight_value = 0.1;    // TODO : should be parameterized ?
+        weights.slice_mut(s![-1, ..]).fill(bias_weight_value);
+
+
         Self {
             neurons,
-            weights: get_random_weights_and_bias_matrix(inputs, neurons),
+            weights,
             activities: Array2::<f64>::zeros((1, 1)),
             activation_function,
         }
@@ -31,26 +40,9 @@ impl Layer {
         let mut output = input_with_bias.dot(&self.weights);
 
         // Apply activation function
-        self.activities = match self.activation_function {
-            Activation::TanH => {
-                output.map(|v| v.tanh())
-            },
-            _ => unreachable!(),
-        };
+        self.activities = self.activation_function.compute(output);
 
+        // Return layer result
         &self.activities
-
     }
-}
-
-fn get_random_weights_and_bias_matrix(rows: usize, columns: usize) -> Array2<f64> {
-
-    // Create matrix with random weights and add one row for the bias weight
-    let mut weights = Array2::<f64>::random((rows + 1, columns), Range::new(0.0, 1.0));
-
-    // Initialize the last column of each row with the bias weight (zero or close to zero)
-    let bias_weight_value = 0.1;
-    weights.slice_mut(s![.., -1]).fill(bias_weight_value);
-
-    weights
 }
